4f4ffdeb2e36cb391b3e7feec565a8963133088c
John Doe <johndoe@example.com>
2023-11-06 17:02:39 +0100

    esercizio-oggi

---
+++ m2/s3/d1/m3s3esercizio/script.js
@@ -0,0 +1,26 @@
+
+
+{
+class User {
+
+    constructor(name,last,age,loco){
+
+        this.name=n;
+        this.last=l;
+        this.age=a;
+        this.loco=lu;
+
+        this.mostraDettagliUser();
+    }
+
+        mostraDettagliUser(){
+            console.log(` mi chiamo ${this.name,this.last} e ho ${ this.age} anni, vivo a ${this.loco}`) ;
+        }
+
+
+}
+
+let uomo1 = new User(andrea, carbone, 27, latina);
+
+console.log(uomo1)
+}
