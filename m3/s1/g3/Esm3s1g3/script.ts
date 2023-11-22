4ae07b8382efa3ff350619184aab0d6ea2a4e80f
John Doe <johndoe@example.com>
2023-11-22 17:07:02 +0100

    esercizio-oggi

---
+++ m3/s1/g3/Esm3s1g3/script.ts
@@ -0,0 +1,15 @@
+abstract class Conto{
+    constructor(
+       public coefficente:number,
+       public redditoLOrdo:number,
+       public tasseImponibili:number,
+       public tasseIrpef:number
+    ){}
+    utileTasse
+
+}
+
+
+class ContoMio extends Conto {
+    sa
+}
