422ccb1af4a0082e1df2f90e0dd8ac8ce7086f5c
John Doe <johndoe@example.com>
2023-11-23 01:09:17 +0100

    esercizio

--- m3/s1/g3/Esm3s1g3/script.ts
+++ m3/s1/g3/Esm3s1g3/script.ts
@@ -1,4 +1,4 @@
-abstract class Conto{
+/*abstract class Conto{
     constructor(
        public coefficente:number,
        public redditoLOrdo:number,
@@ -12,4 +12,56 @@ abstract class Conto{
 
 class ContoMio extends Conto {
     sa
+}*/
+
+abstract class CalcoloTasse{
+
+    tasseIrpef: number = 15;
+    tasseInps: number = 24;
+
+
+    constructor(
+        protected reddAnnuo:number = 0,
+        protected codRedd:number
+        ){};
+
+
+          abstract getUtileTasse(): number
+          abstract getTasseInps(): number
+          abstract getTasseIrpef(): number
+          abstract getReddAnnuoNetto(): number
+} 
+
+
+class WebDeveloper extends CalcoloTasse {
+
+constructor(protected redAnnuo:number = 0){
+    super(redAnnuo,78);
+}
+
+
+getUtileTasse()
+
+
+getRedditoAnnuoNetto(): number {
+    return this.redAnnuo * this.codRedd/ 200
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
